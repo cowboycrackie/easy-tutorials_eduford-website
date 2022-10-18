5a3b8c87ec68f51ffb75bba7662e757905044b45
cowboycrackie <112209004+cowboycrackie@users.noreply.github.com>
2022-10-18 22:38:29 +0200

    menu and mobile menu fix

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var navLinks = document.querySelector('.header nav .nav-list');
+var hamburger = document.querySelector('.header .open-icon .fa');
+var menuIconClose = document.querySelector('.header .nav-list .close-icon .fa');
+var mobileMenuItem = document.querySelectorAll('.header .nav-list ul li a');
 
 function showMenu(){
   navLinks.style.right = "0";
@@ -7,3 +10,30 @@ function showMenu(){
 function hideMenu(){
   navLinks.style.right = "-200px";
 }
+
+hamburger.addEventListener('click', ()=>{
+  hamburger.classList.toggle('inactive');
+  menuIconClose.classList.toggle('inactive');
+})
+
+menuIconClose.addEventListener('click', ()=>{
+  menuIconClose.classList.toggle('inactive');
+  hamburger.classList.toggle('inactive');
+})
+
+document.addEventListener('scroll', ()=>{
+  var scroll_position = window.scrollY;
+
+  if (scroll_position > 600) {
+    hamburger.style.color = 'var(--brand-red)';
+  } else {
+    hamburger.style.color = 'var(--text-color-white)';
+  }
+})
+
+mobileMenuItem.forEach((item)=>{
+  item.addEventListener('click', ()=>{
+    hamburger.classList.toggle('inactive');
+    mobileMenu.classList.toggle('inactive');
+  })
+})
