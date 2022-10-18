06f6ef5cbef240db37c1cc5c116473194f6ad049
cowboycrackie <112209004+cowboycrackie@users.noreply.github.com>
2022-10-18 17:04:20 +0200

    header section

---
+++ app.js
@@ -0,0 +1,9 @@
+var navLinks = document.querySelector('.header nav .nav-list');
+
+function showMenu(){
+  navLinks.style.right = "0";
+}
+
+function hideMenu(){
+  navLinks.style.right = "-200px";
+}
